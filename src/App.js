b4ded433a82bb9592ced8a819f3e57a33c63fe14
marcormun <mcordontrabajo@gmail.com>
2022-07-19 17:28:28 +0200

    Web primera version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import NavBar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
 function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Hello</h1>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Work/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBDPWywpFYXtwb0biy5XKx68sU0yKcYFAo",
+    authDomain: "marcormun-portfolio.firebaseapp.com",
+    projectId: "marcormun-portfolio",
+    storageBucket: "marcormun-portfolio.appspot.com",
+    messagingSenderId: "967026273084",
+    appId: "1:967026273084:web:f8d6f044dc8659f49b4c39",
+    measurementId: "G-LXEHLSR6ZK"
+  };
+  
+  // Initialize Firebase
+initializeApp(firebaseConfig);
